0bbf53d311d9e4503118d7f5452c184e84cd0067
Emir Çavdar <emircvdar@gmail.com>
2024-12-03 20:15:33 +0300

    notification badge for friend request count

--- client/src/app/workspaces/_components/NotificationPopover.tsx
+++ client/src/app/workspaces/_components/NotificationPopover.tsx
@@ -12,11 +12,19 @@ export default function NotificationPopover(authId: any) {
     const { friendRequests, isLoadingFriendRequests, isErrorFriendRequests } = GetFriendRequestsByReceiverId(authId.authId);
     const { allUsers, isLoadingAllUsers, isErrorAllUsers } = GetAllUsers();
     const filteredUsers = allUsers?.filter((user: any) => friendRequests?.some((request: any) => request.sender_id === user.id));
+    const notificationCount = filteredUsers?.length || 0;
 
     return (
         <Popover>
             <PopoverTrigger asChild className="flex items-center gap-2">
-                <Button variant="ghost" size="icon"> <Bell size={12} className="text-white" /></Button>
+                <Button variant="ghost" size="icon" className="relative">
+                    <Bell size={24} className="text-white" />
+                    {notificationCount > 0 && (
+                        <span className="absolute top-0 right-0 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+                            {notificationCount}
+                        </span>
+                    )}
+                </Button>
             </PopoverTrigger>
             <PopoverContent className="w-80">
                 <div className="grid gap-4">
@@ -25,7 +33,7 @@ export default function NotificationPopover(authId: any) {
                         <Separator />
                         {filteredUsers?.map((user: any) => (
                             <div key={user.id} className="flex items-center justify-between gap-2 rounded-md p-2">
-                                <div className="flex">
+                                <div className="flex ">
                                     <p className="text-sm"> <span className="font-bold">{user.fullname}</span> sent you a friend request</p>
                                     <div className="flex items-center gap-2">
                                         <Button variant="ghost" size="icon"> <Check size={12} className="text-green-500" /></Button>
