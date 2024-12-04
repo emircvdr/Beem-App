a0bf7fb18803930760f793cdd227f7bb6b172bae
Emir Çavdar <emircvdar@gmail.com>
2024-12-04 14:43:04 +0300

    .

--- client/src/app/workspaces/_components/Friends.tsx
+++ client/src/app/workspaces/_components/Friends.tsx
@@ -1,10 +1,8 @@
 import { SearchInput } from "@/components/ui/searchInput";
-import { SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
-import deneme from "../../../../public/deneme.jpeg";
+import { SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu } from "@/components/ui/sidebar";
 import FriendsBox from "./FriendsBox";
 import NotificationPopover from "./NotificationPopover";
 import PendingInvitesPopover from "./PendingInvitesPopover";
-import { useEffect } from "react";
 import { AddFriendDialog } from "./AddFriendDialog";
 import { GetFriends } from "@/api/friendsAPI/api";
 import { GetAllUsers } from "@/api/userAPI/api";
@@ -14,10 +12,6 @@ export default function Friends(authId: any) {
     const { allUsers, isErrorAllUsers, isLoadingAllUsers } = GetAllUsers();
     const { friends, isLoadingFriends, isErrorFriends } = GetFriends(authId.authId);
     const filteredUsers = allUsers?.filter((item: any) => item.id !== authId.authId)
-
-    useEffect(() => {
-        console.log(filteredUsers)
-    }, [])
 
     return (
         <SidebarContent>
@@ -33,9 +27,11 @@ export default function Friends(authId: any) {
                     <SearchInput type="search" placeholder="Search" className="mb-5 mt-3 border-none bg-[#6668a0] text-white  placeholder-black" />
                     <SidebarMenu>
                         {
-                            friends?.map((item: any) => (
+                            friends?.length > 0 ? (friends?.map((item: any) => (
                                 <FriendsBox key={item.id} image={item.img?.src} name={filteredUsers.find((user: any) => user.id == item.friend_id || user.id == item.user_id).fullname} id={item.id} userId={filteredUsers.find((user: any) => user.id == item.friend_id || user.id == item.user_id).id} />
-                            ))
+                            ))) : <div className="flex justify-center items-center h-full">
+                                <p className="text-gray-200">No friends found</p>
+                            </div>
                         }
                     </SidebarMenu>
                 </SidebarGroupContent>
